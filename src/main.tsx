c77b1a46c97f8e327b10b2f3cc40cf5881501520
MoniqueeSilva <moniquedasilva2005@gmail.com>
2026-06-30 17:20:19 -0300

    fix: integra rotas da main com autenticação Firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import "./styles/index.css";
 
-import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
 
-import { AuthProvider }
-from "./contexts/AuthContext";
-
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
-
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>
-
 );
